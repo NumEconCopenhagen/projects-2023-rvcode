6a2d8618329fb50d399d2ab2ba9d376be55970a1
victorlundgaard <mwg378@alumni.ku.dk>
2023-03-22 11:40:30 +0100

    Opdateret H

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -58,7 +58,7 @@ class HouseholdSpecializationModelClass:
         elif par.sigma == 1:
             H = HM**(1-par.alpha)*HF**par.alpha
         else:
-            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**((par.sigma-1)/par.sigma)
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**((par.sigma)/(par.sigma-1))
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
