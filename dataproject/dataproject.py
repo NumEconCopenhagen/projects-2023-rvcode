21e8be9c6fb1113dacf99fcf607e265f6886a312
victorlundgaard <mwg378@alumni.ku.dk>
2023-04-12 13:17:06 +0200

    Gini coefficient

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,12 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
-
-    Args:
-        df (pd.DataFrame): pandas dataframe 
-
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+# Define a function to calculate the Gini coefficient
+def gini_coefficient(data):
+    # Calculate the Lorenz curve
+    x, y = lorenz_curve(data)
+    # Calculate the area between the Lorenz curve and the diagonal line of perfect equality
+    area = np.trapz(y, x) - 0.5
+    # Calculate the total area under the diagonal line of perfect equality
+    total_area = 0.5 * x[-1]
+    # Calculate the Gini coefficient as half of the ratio of the area between the Lorenz curve and the diagonal line to the total area
+    gini = area / total_area
+    # Return the Gini coefficient
+    return gini
